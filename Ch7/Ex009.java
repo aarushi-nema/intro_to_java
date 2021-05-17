beb9a98b5ddd3a5af6195133c61e8789270ac759
aarushi_nema02 <63093995+aarushi-nema@users.noreply.github.com>
2021-05-17 16:59:17 +0530

    Update Ex009.java

--- Ch7/Ex009.java
+++ Ch7/Ex009.java
@@ -28,7 +28,7 @@ public class Ex9 {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter 10 elements of the array: ");
         for(int i=0; i<array.length; i++){
-            array[i]= sc.nextInt();
+            array[i]= sc.nextDouble();
         }
     }
 
